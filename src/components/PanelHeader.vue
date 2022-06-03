d62f8c6d2157f717d6aa96b33e156b22e4ffa44c
tdfranklinaz <tdfranklinaz@gmail.com>
2022-06-03 05:01:34 -0700

    init commit

---
+++ src/components/PanelHeader.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Panel Header</h1>
+</template>
---
+++ src/views/PanelView.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import PanelHeader from '@/components/PanelHeader.vue';
+  import PanelSection from '@/components/PanelSection.vue';
+  import PanelSettings from '@/components/PanelSettings.vue';
+</script>
+
+<template>
+  <main>
+    <PanelHeader />
+
+    <div class="container">
+      <h1>Summit Panel</h1>
+    </div>
+
+  </main>
+</template>
+
+<style lang="scss">
+  @import '~milligram/dist/milligram.css';
+
+  h1 { color: #1e90ee; }
+</style>
