33c06d5f29eda22c2d9471e21248c5ce7f94f17e
tdfranklinaz <tdfranklinaz@gmail.com>
2022-06-04 06:10:27 -0700

    working mounted component lifecycle for getting data from nodejs server

--- client/src/views/PanelView.vue
+++ client/src/views/PanelView.vue
@@ -1,14 +1,29 @@
-<script setup>
-  import PanelHeader from '@/components/PanelHeader.vue';
-  //import PanelSection from '@/components/PanelSection.vue';
-  //import PanelSettings from '@/components/PanelSettings.vue';
+<script>
+  export default {
+    name: "Events",
+
+    data() {
+      return {
+        myEvents: []
+      };
+    },
+
+    mounted() {
+      fetch('http://localhost:4000/events')
+        .then(response => response.json())
+        .then(data => (this.myEvents = data));
+    }
+  };
 </script>
 
 <template>
   <main>
     <div class="container">
-      <h1>Summit Panel</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus esse sit expedita ad ab voluptas qui perferendis. Laudantium impedit earum numquam tempora velit ratione maxime, non, iure cumque consequuntur quo?</p>
+
+      <div v-for="events in myEvents" :key="events.id" class="events-data">
+        <p>{{ events.name }}<br/>{{ events.category }}<br/>{{ events.description }}</p>
+      </div>
+
     </div>
   </main>
 </template>
--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-
+const os = require('node:os');
 const app = express();
 
-app.use(morgan('tiny'));
-app.use(cors());
-app.use(bodyParser.json());
+
+
+/**
+ * information about the host system
+ * to be displayed in the user panel
+ */
+let systemInfo = os.platform() + ' ' + os.arch();
 
 app.get('/', (req, res) => {
-  res.json({
-    message: 'nodejs test'
-  });
+  res.json(systemInfo);
 });
 
+
+
+/**
+ * events is custom test data used while
+ * learning vue lifecycle methods
+ */
+let events = [
+  {
+    id: 1,
+    name: 'some event',
+    category: 'some category',
+    description: 'some description',
+  },
+  {
+    id: 2,
+    name: 'second some event',
+    category: 'second some category',
+    description: 'second some description',
+  }
+];
+
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
+app.get('/events/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const event = events.find(event => event.id === id);
+  res.send(event);
+});
+
+
+
+/**
+ * app.listen starts the server on port
+ * define any port you would like
+ */
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
